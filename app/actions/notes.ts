c16f7306d1343fd9c8157e0d81dd839624f87f49
David <davidgonzalezj97@gmail.com>
2026-06-12 19:18:23 +0200

    Server side changing note importance (url based)

--- app/actions/notes.ts
+++ app/actions/notes.ts
@@ -2,13 +2,20 @@
 
 import { redirect } from "next/navigation"
 import { revalidatePath } from "next/cache"
-import { addNote } from "../services/notes"
+import { addNote, toggleImportance } from "../services/notes"
 
 export const createNote = async (formData: FormData) => {
   const content = formData.get("content") as string
   const important = formData.get("important") === "on"
   addNote(content, important)
-  
+
   revalidatePath("/notes")
   redirect("/notes")
+}
+
+export const toggleNoteImportance = async (formData: FormData) => {
+  const id = Number(formData.get("id"))
+  toggleImportance(id)
+  revalidatePath(`/notes/${id}`)
+  revalidatePath("/notes")
 }
